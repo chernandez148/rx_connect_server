40890273471ea69fe8bf35cbe53d5efabacf9585
chernandez148 <66753093+chernandez148@users.noreply.github.com>
2025-04-29 18:38:20 -0700

    Update user_routes.py

--- api/routes/user_routes.py
+++ api/routes/user_routes.py
@@ -1,7 +1,7 @@
 # server/api/routes/user_routes.py
 
 from flask import Blueprint
-from api.controllers.user_controller import CreateUser, GetUsersByPharmacyID
+from api.controllers.user_controller import CreateUser, GetUsersByPharmacyID, GetUserByID, PatchUserByID
 from flask_restful import Api
 
 user_bp = Blueprint('user_bp', __name__)
@@ -10,3 +10,5 @@ api = Api(user_bp)
 # Register Resource
 api.add_resource(CreateUser, "/create_user")
 api.add_resource(GetUsersByPharmacyID, "/<int:pharmacy_id>/users")  # <-- Fixed this line
+api.add_resource(GetUserByID, '/users/<int:user_id>')
+api.add_resource(PatchUserByID, '/users/<int:user_id>')
